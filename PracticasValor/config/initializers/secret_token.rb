ad0e85ae992541105d2513d2211ff2d7702ca37a
unknown <Marco@.(none)>
2012-04-07 12:06:56 -0600

    commit new project

---
+++ PracticasValor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticasValor::Application.config.secret_token = 'e260434818097ee73ae2a059ac61b305db8085238f3a6d014ee5fdb6386b7386373a97a6b63a5fb7225e000f0bfeb0dd436945c85d1742df0e3a5c106288893c'
